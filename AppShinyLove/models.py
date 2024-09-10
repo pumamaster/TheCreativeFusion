cf7c1e729ce67bf9351ccb0166eaa303b41e8a61
Alejandro González <alejandrogongis03@gmail.com>
2024-09-10 08:55:05 -0600

    Cambios en imágenes

--- AppShinyLove/models.py
+++ AppShinyLove/models.py
@@ -5,6 +5,7 @@ class Photo(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = CloudinaryField('image')
+    vertical = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
